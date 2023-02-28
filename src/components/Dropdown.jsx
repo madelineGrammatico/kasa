b964e57d37170f337dd20a86e8c13a657e737d0b
Madeline <grammaticomadeline@hotmail.fr>
2023-02-28 23:48:25 +0100

    fix(app) : add destructuring in dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,28 +2,25 @@ import { useState } from "react"
 import { ReactComponent as DownChevron} from "../asset/chevron-down-solid.svg"
 import { ReactComponent as UpChevron } from "../asset/chevron-up-solid.svg"
 
-export default function Dropdown(props) {
-  const isOpenState = useState(false)
-  const isOpen= isOpenState[0]
-  const setIsOpen = isOpenState[1]
-  // const { isOpen, setIsOpen } = useState(false)
+export default function Dropdown({title, body}) {
+  const [ isOpen, setIsOpen ] = useState(false)
 
   return isOpen?(
     <article className="dropdown">
         <header className="dropdown__header" onClick={() => setIsOpen(false)}>
-            <h2>{props.title}</h2> <UpChevron className="up" alt="caché infos"/>
+            <h2>{title}</h2> <UpChevron className="up" alt="caché infos"/>
         </header>
-          { typeof(props.body)==="object"? (
+          { typeof(body)==="object"? (
             <ul className="dropdown__body">
-              { props.body.map((item, index) => <li key={index} className="dropdown__body__item">{item}</li>) }
+              { body.map((item, index) => <li key={index} className="dropdown__body__item">{item}</li>) }
             </ul>
             ) : (
-            <p className="dropdown__body" >{props.body}</p>) }
+            <p className="dropdown__body" >{body}</p>) }
     </article>
   ) : (
     <article className="dropdown">
         <header className="dropdown__header" onClick={() => setIsOpen(true)}>
-            <h2>{props.title}</h2> <DownChevron className="down" alt="ouvrir infos"/>
+            <h2>{title}</h2> <DownChevron className="down" alt="ouvrir infos"/>
         </header>
     </article>
   )
