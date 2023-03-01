5db928c575ded33161aac0061b56b630f99cdbcf
Madeline <grammaticomadeline@hotmail.fr>
2023-03-01 13:06:58 +0100

    fix(app) : fix text banner and add destructuring

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,14 @@
 
 
-export function Banner(props) {
-  const {banner, text} = props
+export function Banner({banner, text}) {
   return (
     <section className="banner">
         <div className="banner__shadow"></div>
-        <img src={banner} className="banner__img" alt="falaise"/>
-        { text && <h1>{text}</h1 >}
+        <img src={banner} className="banner__img" alt={banner.split("/").slice(-1)}/>
+        { text && text.length ===1 ? 
+          <h1>{text}</h1 > 
+        : 
+          <h1>{text.map((item, index) => <span key={index}>{item}</span>)}</h1>}
     </section>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import bannerHome from "../asset/banner_Home.png"
 function Home() {
   return (
     <main className="Home">
-      <Banner banner ={bannerHome} text="Chez vous, partout et ailleurs"/>
+      <Banner banner ={bannerHome} text={["Chez vous,"," partout et ailleurs"]}/>
       <Gallery/>
     </main>
   );
