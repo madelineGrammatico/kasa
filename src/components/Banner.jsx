4fa39d4232487def3f9d0ad37634ea53c49a3be6
Madeline <grammaticomadeline@hotmail.fr>
2023-03-01 23:26:18 +0100

    fix(app) : fix banner when no text

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,17 @@
 
 
-export function Banner({banner, text}) {
+export function Banner({banner, text=""}) {
+  console.log(text)
   return (
     <section className="banner">
         <div className="banner__shadow"></div>
         <img src={banner} className="banner__img" alt={banner.split("/").slice(-1)}/>
-        { text && text.length ===1 ? 
-          <h1>{text}</h1 > 
-        : 
-          <h1>{text.map((item, index) => <span key={index}>{item}</span>)}</h1>}
+        { 
+          (text.length !== 0) && (text.length >=1) ? 
+            <h1>{text.map((item, index) => <span key={index}>{item}</span>)}</h1>
+          : 
+            <h1>{text}</h1 > 
+        }
     </section>
   )
 }
